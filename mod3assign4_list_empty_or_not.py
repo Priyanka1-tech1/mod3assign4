a1a436ec42e6bb848f791f428b45c824de5e506a
Priyanka1-tech1 <67084288+Priyanka1-tech1@users.noreply.github.com>
2020-06-27 18:34:08 +0530

    Add files via upload

---
+++ mod3assign4_list_empty_or_not.py
@@ -0,0 +1,7 @@
+l=[ ]
+if len(l)==0:
+	print('list is empty')
+else:
+	print('list is not empty')
+
+    
---
+++ mod3assign4_min_max.py
@@ -0,0 +1,3 @@
+l=[1,3,5,7,6,2,9,10,11,0]
+print('maximum',max(l))
+print('minimum',min(l))
